5cc8d6f747cef95485c8a44a472014333b32bddd
Jacob Robinson <robinson.jacob.e@gmail.com>
2024-11-03 19:40:05 +0000

    Rendered PaginatedList in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import PaginatedList from "@/components/PaginatedList";
+import QueryWrapper from "../components/QueryWrapper";
+import { IPerson, IPeopleResponse } from "@/types/global";
+import PersonCard from "@/components/PersonCard";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold">SWAPI Example</h1>
+    <main className="flex min-h-screen flex-col items-center p-24 gap-6">
+      <QueryWrapper>
+        <h1 className="text-5xl font-bold">SWAPI Example</h1>
+        <PaginatedList
+          query="https://swapi.dev/api/people"
+          renderItem={(person: IPerson) => <PersonCard person={person} />}
+          dataExtractor={(data: IPeopleResponse) => data.results}
+          paginationExtractor={(data: IPeopleResponse) => ({
+            totalCount: data.count,
+          })}
+          searchKey="search"
+        />
+      </QueryWrapper>
     </main>
   );
 }
