4e9f3f61ebb27609ac956d902edeffed71105886
Jacob Robinson <robinson.jacob.e@gmail.com>
2024-11-04 20:47:09 +0000

    Fixes to useFavourite hook

--- src/hooks/useFavourite.ts
+++ src/hooks/useFavourite.ts
@@ -4,25 +4,27 @@ export default function useFavourite(
   id: string
 ): readonly [boolean, () => void] {
   const [favourites, setFavourites] = useState<Set<string>>(new Set());
+  const [isFavourite, setIsFavourite] = useState(false);
 
   useEffect(() => {
     const storedFavourites = localStorage.getItem("favourites");
     if (storedFavourites) {
       setFavourites(new Set(JSON.parse(storedFavourites)));
+      setIsFavourite(new Set(JSON.parse(storedFavourites)).has(id));
     } else {
       localStorage.setItem("favourites", JSON.stringify([]));
     }
   }, []);
-
-  const isFavourite = favourites.has(id);
 
   function toggleFavourite() {
     const updatedFavourites = new Set(favourites);
 
     if (updatedFavourites.has(id)) {
       updatedFavourites.delete(id);
+      setIsFavourite(false);
     } else {
       updatedFavourites.add(id);
+      setIsFavourite(true);
     }
 
     setFavourites(updatedFavourites);
